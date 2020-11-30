ff1e6fc0aa88b3bffdcaaa18b183ef8d9afc2fca
nico <nicolas.pires@gmail.com>
2020-11-30 22:22:02 +0100

    Mise en forme index

--- js/index.js
+++ js/index.js
@@ -1,56 +1,37 @@
-/*
+var get = function (url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new window.XMLHttpRequest()
 
-const elt = document.getElementById('panier');    // On récupère l'élément sur lequel on veut détecter le clic
-elt.addEventListener('click', function() {          // On écoute l'événement click
-alert ("Dans le panier");               // On change le contenu de notre élément pour afficher "C'est cliqué !"
-});
-
-
-
-const selectElement = document.querySelector('.choix-nounours'); // On selectionne la classe .choix-nounours ou on veut faire un event
-selectElement.addEventListener('change', (event) => {   // On écoute l'événement change d'etat
-  const result = document.querySelector('.result');     // On selectionne la classe .result ou on veut affichez notre resultat
-  result.textContent = `Vous avez choisis ${event.target.value}`;  // On affiche le contenu de notre event pour afficher "Vous avez choisis"
-});
-
-*/
-/*
-const appelApi = "http://localhost:3000/api/teddies";         // Déclaration de l'Url de l'api
-const bearImg = document.getElementById("main");
-
-async function getTeddies() {                       // Création d'une fonction asynchrone
-    let response = await fetch(appelApi);           // La réponse de cette fonction est d'attendre la réponse (en json) de l'api
-    let data = await response.json()                // Une fois qu'on a la réponse on peut la déclarer (data)
-    .then((data) => {
-        data.forEach((teddy) => {                   // Pour chaque teddy dans data on va créer du contenu HTML
-            const { name, _id, colors, price, description, imageUrl } = teddy      // Déclaration de teddy comme objet
-            bearImg.innerHTML +=
-            `<div class="${name}">
-                <h3 class="teddyName">${name}</h3>
-                <ul class="teddyInfo">
-                    <li id="price">Prix: ${price/100}€</li>
-                </ul>
-                <img src="${imageUrl}" alt="Photo de ${name}" class="teddyPhoto"></img>
-                <button onclick='location.href="product-page.html?id=${_id}"' type="button" class="btn btn-primary" id="btnCustom"><i class="fas fa-paw"></i>Personnaliser mon teddy</button>
-            </div>`; 
-                    
-                                               // Cette partie de récupération de l'id est très
-                                                     // importante pour la page suivante
-        })                                                      
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    let product = JSON.parse(this.responseText);
+                    console.table(product)
+                    resolve(xhr.responseText)
+                    let cardTeddies = document.getElementById('cardTeddies')
+                    product.forEach(teddies => {
+                        cardTeddies.innerHTML += `    
+                    <div class="card text-center"style="width: 450px;">
+                        <img src="${teddies.imageUrl}" class="card-img-top bg-transparent"  alt="...">
+                        <div class="card-body">
+                            <h5 class="font card-title text-center">${teddies.name}</h5>
+                            <p class="card-text">${teddies.description}</p>
+                            <p class="card-text">${(teddies.price/100).toFixed(2)} £</p>
+                                <a href="produit.html?id=${teddies._id}" class="btn btn-primary"><i class="fas fa-cart-arrow-down"></i> Choisis Moi</a>
+                        </div>
+                    </div>
+                    `
+                    });
+                } else {
+                    reject(xhr)
+                }
+            }
+        }
+        xhr.open('GET', url, true)
+        xhr.send()
     })
-    return data;
 }
 
-window.onload = () => {                 // Force le lancement de la fonction getTeddies() au chargement de la page
-    getTeddies();
-}
-*/
-
-
-
-
-
-
-
-
-
+get('http://localhost:3000/api/teddies').then(function (response) {
+    console.log(response)
+})
